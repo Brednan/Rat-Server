7ed87ab99a08c259807b2fc2deb145ed8ab01c1d
Brendan Caldwell <brendanshauncaldwell@gmail.com>
2024-07-21 11:44:27 -0700

    Created ExeFile Entity

---
+++ Rat-Server/Model/Entities/ExeFile.cs
@@ -0,0 +1,9 @@
+﻿namespace Rat_Server.Model.Entities
+{
+    public class ExeFile
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+    }
+}
