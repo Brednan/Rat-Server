615bcabb80687874fe155c6e6dd1e4a9f2ed5b62
brendan-camosun <144830030+brendan-camosun@users.noreply.github.com>
2024-08-03 12:27:28 -0700

    Created AuthenticationControllerTest constructor

---
+++ Controller-Tests/AuthenticationControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rat_Server.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Controller_Tests
+{
+    public class AuthenticationControllerTest : BaseControllerTest
+    {
+        AuthenticationController _controller;
+
+        public AuthenticationControllerTest(ITestOutputHelper output) : base(output)
+        {
+            _controller = new AuthenticationController(_context, _config);
+        }
+    }
+}
