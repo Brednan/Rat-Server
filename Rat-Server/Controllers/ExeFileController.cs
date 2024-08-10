fb40a6d7b27b274aa381c330b16a566028fbe6d8
brendan-camosun <144830030+brendan-camosun@users.noreply.github.com>
2024-08-10 14:50:37 -0700

    Made GetExeFile endpoint return a File instead of an ExeFile instance

--- Rat-Server/Controllers/ExeFileController.cs
+++ Rat-Server/Controllers/ExeFileController.cs
@@ -27,7 +27,7 @@ namespace Rat_Server.Controllers
         }
 
         [HttpGet("GetExeFile/{FileName}")]
-        [ProducesResponseType<ExeFile>(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ExeFile>> GetExeFile(string FileName)
         {
@@ -37,7 +37,7 @@ namespace Rat_Server.Controllers
                 return NotFound();
             }
 
-            return Ok(exeFile);
+            return File(exeFile.Content, "application/octet-stream", exeFile.Name);
         }
     }
 }
